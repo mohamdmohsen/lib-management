7a6a3bc280fe4275c3dafa2efb4f7f8f589a28f0
mohamdmohsen <mohamedmohseen63@gmail.com>
2026-09-23 19:14:23 +0300

    add feature

--- review/views.py
+++ review/views.py
@@ -2,15 +2,12 @@
 from rest_framework.response import Response
 from .serializers import FavoriteSerializer,ReviewSerializer
 from .models import Favorite , Review
-from rest_framework.decorators import api_view ,permission_classes
-from rest_framework import status,generics
-from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+
+
 from rest_framework import viewsets, permissions
 from .models import Review
 from .serializers import ReviewSerializer
 from .permissions import IsOwnerOrReadOnly
-
 
 
 
@@ -32,3 +29,4 @@ class FavoriteViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
